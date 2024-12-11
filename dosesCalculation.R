676ef65d7983e5f8d6bdae22f5c451978440f0c5
Li Li <lli4@fredhutch.org>
2024-12-11 08:02:08 -0800

    Moved xx_iterated_sx.R to archive

--- dosesCalculation.R
+++ dosesCalculation.R
@@ -9,7 +9,7 @@ source(here::here("utils.R"))
 
 # Input parameters --------------------------------------------------------
 
-numOfDosesTab <- tibble("rate_pla" = numeric(), "n_target_cases" = numeric(), 
+numOfDosesTab1 <- tibble("rate_pla" = numeric(), "n_target_cases" = numeric(), 
                         "n_ab" = numeric(), "n_pla" = numeric(),
                         "nullHR" = numeric(), "altHR" = numeric(), "tau" = numeric(), 
                         "minNumOfDoses" = numeric(),
@@ -20,28 +20,17 @@ numOfDosesTab <- tibble("rate_pla" = numeric(), "n_target_cases" = numeric(),
                         "maxNumOfDoses" = numeric())
 
 # 2-arm designs and low-dose vs pla comparisons in 3-arm designs
-# n_target_cases <- c(23, 23, 43, 135)
-# rate_pla <- 0.026
-# nullHR <- 0.75
-# altHR <- c(0.15, 0.15, 0.25, 0.35)
-# rate_cens <- 0.075
-# p_ab <- 0.5
-# p_pla <- 0.5
-# n_enroll_4m <- c(1000, 1000, 800, 800)
-# tau <- c(1, rep(1.5, 3))
-# iter <- 2000
-
-# high-dose vs pla comparisons in 3-arm designs
-n_target_cases <- c(37, 108)
+n_target_cases <- c(23, 23, 43, 135)
 rate_pla <- 0.026
 nullHR <- 0.75
-altHR <- c(0.15, 0.15)
+altHR <- c(0.15, 0.15, 0.25, 0.35)
+n_enroll_4m <- c(1000, 1000, 800, 800)
 rate_cens <- 0.075
-p_ab <- 1/3
-p_pla <- 2/3
-n_enroll_4m <- c(600, 600)
-tau <- c(1.5, 1.5)
+p_ab <- 0.5
+p_pla <- 0.5
+tau <- c(1, rep(1.5, 3))
 iter <- 2000
+
 
 for (i in 1:length(n_target_cases)){
   df <- oper_chars_eff_phase_dosesCalculation_twoArms(n_target_cases = n_target_cases[i],
@@ -55,7 +44,7 @@ for (i in 1:length(n_target_cases)){
                                                       tau = tau[i],
                                                       iter = iter)
   
-  numOfDosesTab <- add_row(.data = numOfDosesTab, "rate_pla" = rate_pla,
+  numOfDosesTab1 <- add_row(.data = numOfDosesTab1, "rate_pla" = rate_pla,
                            "n_target_cases" = n_target_cases[i], 
                            "n_ab" = df$n_ab[1],
                            "n_pla" = df$n_pla[1],
@@ -68,4 +57,53 @@ for (i in 1:length(n_target_cases)){
                            "maxNumOfDoses" = max(df$numOfDoses))
 }
 
-numOfDosesTab
+
+# # high-dose vs pla comparisons in 3-arm designs
+n_target_cases <- c(37, 108)
+rate_pla <- 0.026
+nullHR <- 0.75
+altHR <- c(0.15, 0.15)
+rate_cens <- 0.075
+p_ab <- 1/3
+p_pla <- 2/3
+n_enroll_4m <- c(600, 600)
+tau <- c(1.5, 1.5)
+iter <- 2000
+
+numOfDosesTab2 <- tibble("rate_pla" = numeric(), "n_target_cases" = numeric(), 
+                         "n_ab" = numeric(), "n_pla" = numeric(),
+                         "nullHR" = numeric(), "altHR" = numeric(), "tau" = numeric(), 
+                         "minNumOfDoses" = numeric(),
+                         "Q1NumOfDoses" = numeric(),
+                         "meanNumOfDoses" = numeric(),
+                         "Q2NumOfDoses" = numeric(),
+                         "Q3NumOfDoses" = numeric(),
+                         "maxNumOfDoses" = numeric())
+
+for (i in 1:length(n_target_cases)){
+  df <- oper_chars_eff_phase_dosesCalculation_twoArms(n_target_cases = n_target_cases[i],
+                                                      rate_pla = rate_pla,
+                                                      nullHR = nullHR,
+                                                      altHR = altHR[i],
+                                                      rate_cens = rate_cens,
+                                                      p_ab = p_ab,
+                                                      p_pla = p_pla,
+                                                      n_enroll_4m = n_enroll_4m[i],
+                                                      tau = tau[i],
+                                                      iter = iter)
+  
+  numOfDosesTab2 <- add_row(.data = numOfDosesTab2, "rate_pla" = rate_pla,
+                           "n_target_cases" = n_target_cases[i], 
+                           "n_ab" = df$n_ab[1],
+                           "n_pla" = df$n_pla[1],
+                           "nullHR" = nullHR, "altHR" = altHR[i], "tau" = tau[i], 
+                           "minNumOfDoses" = min(df$numOfDoses),
+                           "Q1NumOfDoses" = quantile(df$numOfDoses, probs = 0.25),
+                           "meanNumOfDoses" = mean(df$numOfDoses),
+                           "Q2NumOfDoses" = quantile(df$numOfDoses, probs = 0.5),
+                           "Q3NumOfDoses" = quantile(df$numOfDoses, probs = 0.75),
+                           "maxNumOfDoses" = max(df$numOfDoses))
+}
+
+numOfDosesTab1
+numOfDosesTab2
